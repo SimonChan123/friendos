86c0d2e3cb0618dd507ea8068bc401dcc0eceed2
Simon Chan <schan92@hotmail.com>
2019-11-27 19:50:25 -0800

    firebase middleware authorization added => verified users need to be logged in before posting

--- functions/index.js
+++ functions/index.js
@@ -35,13 +35,51 @@ app.get('/posts', (request, response) => {
             });
             return response.json(posts);
         })
-        .catch((err) => console.error(err));
+        .catch((err) => {
+            console.error(err);
+            response.status(500).json({ error: err.code });
+        });
 });
 
-app.post('/posts', (request, response) => {
+// middleware
+const FBAuth = (request, response, next) => {
+    let idToken;
+    if (request.headers.authorization && request.headers.authorization.startsWith('Bearer ')) {
+        idToken = request.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found');
+        return response.status(403).json({ error: 'Unauthorized' });
+    }
+
+    // verify token is issued by my application
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            request.user = decodedToken;
+            console.log(decodedToken);
+            return db.collection('users')
+                .where('userId', '==', request.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then(data => {
+            request.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch((err) => {
+            console.error('Error while verifying token ', err);
+            return response.status(403).json(err);
+        });
+};
+
+// posts route
+app.post('/posts', FBAuth, (request, response) => {
+    if (request.body.body.trim() === '') {
+        return response.status(400).json({ body: 'Must not be empty.' });
+    }
+
     const newPost = {
         body: request.body.body,
-        userHandle: request.body.userHandle,
+        userHandle: request.user.handle,
         createdAt: new Date().toISOString()
     };
 
